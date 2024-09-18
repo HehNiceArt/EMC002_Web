232a469ce359e413731f73df2846a0e0bcd1473e
cicerolb <115563856+cicerolb@users.noreply.github.com>
2024-09-18 23:14:45 +0800

    imageslider

---
+++ src/components/HomePictures.js
@@ -0,0 +1,33 @@
+// src/ImageSlider.js
+import React, { useState } from 'react';
+import './css/ImageSlider.css';
+import poolRoom from './pictures/pool-room.jpg'
+import restaurantRoom from './pictures/restaurant-room.jpg'
+import hotelRoom from './pictures/hotel-room.jpg'
+
+
+const ImageSlider = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const images = [
+    poolRoom, restaurantRoom, hotelRoom
+  ];
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  };
+
+  return (
+    <div className="slider">
+      <button className="prev" onClick={prevSlide}>❮</button>
+      <img src={images[currentIndex]} className='imageSlider' alt={`Slide ${currentIndex + 1}`} />
+      <button className="next" onClick={nextSlide}>❯</button>
+    </div>
+  );
+};
+
+export default ImageSlider;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import HomePictures from "../components/HomePictures";
 
 /* Cicero Barasi */
 export default function HomePage() {
     return (
         <div>
             <Navbar />
-            Home Page
+            {/* Home Page */}
+            <HomePictures />
         </div>
     )
 }
