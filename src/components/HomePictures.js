5cfd21a2e01866c5679c548374f4f836722b6026
cicerolb <115563856+cicerolb@users.noreply.github.com>
2024-09-18 23:39:07 +0800

    register segment

--- src/components/HomePictures.js
+++ src/components/HomePictures.js
@@ -4,6 +4,7 @@ import './css/ImageSlider.css';
 import poolRoom from './pictures/pool-room.jpg'
 import restaurantRoom from './pictures/restaurant-room.jpg'
 import hotelRoom from './pictures/hotel-room.jpg'
+import registerImage from './pictures/register-img.png'
 
 
 const ImageSlider = () => {
@@ -22,11 +23,19 @@ const ImageSlider = () => {
   };
 
   return (
-    <div className="slider">
-      <button className="prev" onClick={prevSlide}>❮</button>
-      <img src={images[currentIndex]} className='imageSlider' alt={`Slide ${currentIndex + 1}`} />
-      <button className="next" onClick={nextSlide}>❯</button>
+    <div>
+        <div className="slider">
+            <button className="prev" onClick={prevSlide}>❮</button>
+            <img src={images[currentIndex]} className='imageSlider' alt={`Slide ${currentIndex + 1}`} />
+            <button className="next" onClick={nextSlide}>❯</button>
+        </div>
+        <div>
+            <img src={registerImage} alt='Register' className='registerImage'/>
+            <p className='RegisterTitle'>A place Between Dreams and Reality</p>
+        </div>
     </div>
+    
+    
   );
 };
 
