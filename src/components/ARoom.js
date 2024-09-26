a3fe608cf7d7aa633148af1ceb238d476e2d9cf6
HehNice <122663831+HehNiceArt@users.noreply.github.com>
2024-09-26 12:41:42 +0800

    admin

--- src/components/ARoom.js
+++ src/components/ARoom.js
@@ -2,23 +2,63 @@ import React from "react";
 import AdminNavbar from "./AdminNavbar";
 
 import './css/Admin.css'
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 export default function ARoom() {
     return (
         <div className="Admin-Container">
             <AdminNavbar />
-            <div className="Room-List">
-                <div className="Room-Bookings">
-                    <h1 className="Room-Tally">6</h1>
-                    <p>New Bookings</p>
+            <div className="Admin-Content">
+                <div className="Room-List">
+                    <div className="Room-Bookings">
+                        <h1 className="Room-Tally">6</h1>
+                        <p>New Bookings</p>
+                    </div>
+                    <div className="Room-CheckIn">
+                        <h1 className="Room-Tally">8</h1>
+                        <p>Check In</p>
+                    </div>
+                    <div className="Room-CheckOut">
+                        <h1 className="Room-Tally">10</h1>
+                        <p>Check Out</p>
+                    </div>
                 </div>
-                <div className="Room-CheckIn">
-                    <h1 className="Room-Tally">8</h1>
-                    <p>Check In</p>
-                </div>
-                <div className="Room-CheckOut">
-                    <h1 className="Room-Tally">10</h1>
-                    <p>Check Out</p>
-                </div>
+                <Container className="grid-container">
+                    <Row className="grid-line">
+                        <Col className="grid-line"><h2>Code</h2></Col>
+                        <Col className="grid-line"><h2>Room</h2></Col>
+                        <Col className="grid-line"><h2>Total</h2></Col>
+                        <Col className="grid-line"><h2>Name</h2></Col>
+                    </Row>
+                    <Row className="grid-line">
+                        <Col className="grid-line"><p>Code</p></Col>
+                        <Col className="grid-line"><p>Room</p></Col>
+                        <Col className="grid-line"><p>Total</p></Col>
+                        <Col className="grid-line"><p>Name</p></Col>
+                    </Row>
+                    <Row className="grid-line">
+                        <Col className="grid-line"><p>Code</p></Col>
+                        <Col className="grid-line"><p>Room</p></Col>
+                        <Col className="grid-line"><p>Total</p></Col>
+                        <Col className="grid-line"><p>Name</p></Col>
+                    </Row>
+                    <Row className="grid-line">
+                        <Col className="grid-line"><p>Code</p></Col>
+                        <Col className="grid-line"><p>Room</p></Col>
+                        <Col className="grid-line"><p>Total</p></Col>
+                        <Col className="grid-line"><p>Name</p></Col>
+                    </Row>
+                    <Row className="grid-line">
+                        <Col className="grid-line"><p>Code</p></Col>
+                        <Col className="grid-line"><p>Room</p></Col>
+                        <Col className="grid-line"><p>Total</p></Col>
+                        <Col className="grid-line"><p>Name</p></Col>
+                    </Row>
+                </Container>
             </div>
         </div>
     )
