b07e97204af25a335f2cccb54eb2cf5e1d310a62
cicerolb <115563856+cicerolb@users.noreply.github.com>
2024-10-11 01:08:22 +0800

    big update roomView

--- src/components/RoomView.js
+++ src/components/RoomView.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../components/css/RoomView.css";
+import Dropdown from "react-bootstrap/Dropdown";
 
 export default function RoomView() {
   return (
@@ -11,7 +12,7 @@ export default function RoomView() {
         height: "150vh",
       }}
     >
-      <div className="roomImage">
+      <div className="upperRow">
         <img
           src="https://cdnb.artstation.com/p/assets/images/images/027/766/497/large/nihal-devang-finl.jpg?1592488454"
           alt=""
@@ -40,30 +41,46 @@ export default function RoomView() {
         </div>
       </div>
 
-      {/* terms and conditions */}
-      <div className="termsBox">
-        <p style={{ display: "inline", marginRight: "65px", fontSize: "11px" }}>
-          Offer Details
-        </p>
-        <p style={{ display: "inline" }}>Terms and Conditions</p>
-        <p style={{ marginTop: "10px" }}>
-          Get ready to elevate your Liminal experience to dazzling heights!
-          Enjoy an unforgettable celebration where the iconic uncanny valley
-          become a mesmerizing masterpiece just outside your window.
-        </p>
-        <p>
-          Immerse yourself in the magic of Liminal Escapes world-renowned
-          liminal rooms from the comfort of your own haven. Enjoy panoramic
-          views of the liminal spaces as bursts of muted colours and sparkles
-          dance across the night sky.
-        </p>
-        <p>Your bucket-list Liminal Spaces begins here!</p>
-        <p>
-          This offer includes: <br />
-          - Overnight accomodation <br />- High-speed and restriction-free WiFi
-          and broadband internet access
-        </p>
-        <p>Liminal Escapes Cares - Your Well-being In Our Care</p>
+      <div className="lowerRow">
+        {/* terms and conditions */}
+        <div className="termsBox">
+          <p
+            style={{ display: "inline", marginRight: "65px", fontSize: "11px" }}
+          >
+            Offer Details
+          </p>
+          <p style={{ display: "inline" }}>Terms and Conditions</p>
+          <p style={{ marginTop: "10px" }}>
+            Get ready to elevate your Liminal experience to dazzling heights!
+            Enjoy an unforgettable celebration where the iconic uncanny valley
+            become a mesmerizing masterpiece just outside your window.
+          </p>
+          <p>
+            Immerse yourself in the magic of Liminal Escapes world-renowned
+            liminal rooms from the comfort of your own haven. Enjoy panoramic
+            views of the liminal spaces as bursts of muted colours and sparkles
+            dance across the night sky.
+          </p>
+          <p>Your bucket-list Liminal Spaces begins here!</p>
+          <p>
+            This offer includes: <br />
+            - Overnight accomodation <br />- High-speed and restriction-free
+            WiFi and broadband internet access
+          </p>
+          <p>Liminal Escapes Cares - Your Well-being In Our Care</p>
+        </div>
+
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            Dropdown Button
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
       </div>
     </div>
   );
