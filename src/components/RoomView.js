9b8cccf091c666d54c5cf6319a9a62a1e42a832a
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2024-10-10 14:41:50 +0800

    updated RoomView

--- src/components/RoomView.js
+++ src/components/RoomView.js
@@ -1,1 +1,6 @@
 import React from "react" 
+import '../components/css/RoomView.css'
+
+export default function RoomView() {
+    
+}
--- src/pages/RoomViewPage.js
+++ src/pages/RoomViewPage.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import RoomView from "../components/RoomView";
 
 /* Cicero */
 export default function RoomViewPage() {
     return (
         <div>
             <Navbar />
+            <RoomView />
             Room View
-
+            <Footer />
+            
         </div>
     );
 }
