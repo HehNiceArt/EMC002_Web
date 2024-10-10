683d3ea6618860b164a786a595d13f92fd1bc9ee
cicerolb <115563856+cicerolb@users.noreply.github.com>
2024-10-10 20:52:22 +0800

    updated roomview

--- src/components/RoomView.js
+++ src/components/RoomView.js
@@ -1,6 +1,12 @@
-import React from "react" 
-import '../components/css/RoomView.css'
+import React from "react"; 
+import '../components/css/RoomView.css';
 
 export default function RoomView() {
-    
+    return (
+        <div className="backgroundContainer">
+            <img src="https://cdnb.artstation.com/p/assets/images/images/027/766/497/large/nihal-devang-finl.jpg?1592488454" alt="" className="backgroundImage" />
+            <img src="https://cdnb.artstation.com/p/assets/images/images/027/766/497/large/nihal-devang-finl.jpg?1592488454" alt="" className="overlayText" />
+
+        </div>
+    );
 }
