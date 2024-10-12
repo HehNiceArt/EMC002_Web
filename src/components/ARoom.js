5fe920f3ca823cf24d48d61b361379a39a023fbc
HehNice <122663831+HehNiceArt@users.noreply.github.com>
2024-10-12 14:47:17 +0800

    forgot

--- src/components/ARoom.js
+++ src/components/ARoom.js
@@ -6,7 +6,6 @@ import './css/Admin.css'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 export default function ARoom() {
     return (
