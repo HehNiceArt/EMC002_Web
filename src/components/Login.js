9293602a2e16187e9a4370b15bbb922b33b85568
HehNice <122663831+HehNiceArt@users.noreply.github.com>
2024-10-12 20:23:50 +0800

    choices for login or registration

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,10 +6,17 @@ import RegistrationForm from "./RegistrationForm";
 import { useNavigate } from "react-router";
 export default function Login() {
     const [isRegistered, setIsRegistered] = useState(false);
+    const [showForm, setShowForm] = useState(null);
     const navigate = useNavigate();
 
     const navigateToHome = () => {
         navigate('/');
+    }
+    const showLoginForm = () => {
+        setShowForm("login");
+    }
+    const showRegistrationForm = () => {
+        setShowForm("register");
     }
     return (
         <div className="Login-Container">
@@ -20,7 +27,18 @@ export default function Login() {
                             <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#FFFFFF" onClick={navigateToHome}><path d="M650-80 250-480l400-400 61 61.67L372.67-480 711-141.67 650-80Z" /></svg>
                         </div>
                     </div>
-                    {isRegistered ? <LoginForm /> : <RegistrationForm setIsRegistered={setIsRegistered} />}
+                    <div className="Login-Choice">
+                        {showForm === null && (
+                            <div className="buttonContainer">
+                                <h1>Liminal Escapes</h1>
+                                <button onClick={showLoginForm} className="formButton">Login</button>
+                                <button onClick={showRegistrationForm} className="formButton">Register</button>
+                            </div>
+                        )}
+                    </div>
+                    {/* Show the selected form */}
+                    {showForm === "login" && <LoginForm />}
+                    {showForm === "register" && <RegistrationForm setIsRegistered={setIsRegistered} />}
                 </div>
             </div>
         </div>
