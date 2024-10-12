1476c46825c01aa706c1954788202308f1653352
HehNice <122663831+HehNiceArt@users.noreply.github.com>
2024-10-12 21:06:50 +0800

    routes and models

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
+import eventsRoutes from './routes/events.js'
+import reservationsRoutes from './routes/reservations.js'
+import roomsRoutes from './routes/rooms.js'
+import amenitiesRoutes from './routes/amenities.js'
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -18,6 +22,10 @@ mongoose.connect(process.env.MONGODB_URI)
     .then(() => { console.log('Connected to MongoDB') })
     .catch(err => console.log(err));
 
+app.use('/api/events', eventsRoutes);
+app.use('/api/reservations', reservationsRoutes);
+app.use('/api/rooms', roomsRoutes);
+app.use('/api/amenities', amenitiesRoutes)
 app.use('/api', authRoutes);
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
